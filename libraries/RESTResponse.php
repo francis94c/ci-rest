ff5668365f1618e7a2ee18e6b6e80f949d1c4f1a
Francis Ilechukwu <francis94c@gmail.com>
2019-11-09 20:33:36 +0100

    Refactor.

--- libraries/RESTResponse.php
+++ libraries/RESTResponse.php
@@ -1,7 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class RESTResponse {
+class RESTResponse extends CI_Controller
+{
+  // Response Codes
   // 40*
   const BAD_REQUEST           = 400;
   const UN_AUTHORIZED         = 401;
@@ -11,4 +13,59 @@ class RESTResponse {
   // 50*
   const INTERNAL_SERVER_ERROR = 500;
   const NOT_IMPLEMENTED       = 501;
+  /**
+   * [protected HTTP Response Code]
+   * @var int
+   */
+  protected $code;
+  /**
+   * [protected Response Data]
+   * @var mixed
+   */
+  protected $data;
+  /**
+   * [protected Shoud Response be JSON Encoded?]
+   * @var bool
+   */
+  protected $json;
+  function __construct($data, int $code)
+  {
+    $this->data = $data;
+    $this->code = $code;
+  }
+  /**
+   * [__toString description]
+   * @date   2019-11-09
+   * @return string     [description]
+   */
+  public function __toString():string
+  {
+    return !$this->json ? $this->data : n_encode($this->data);
+  }
+  /**
+   * [setJSON description]
+   * @date   2019-11-09
+   * @param  bool       $json [description]
+   * @return RESTResponse         [description]
+   */
+  public function setJSON(bool $json):RESTResponse
+  {
+    $this->json = $json;
+    return $this;
+  }
+  /**
+   * [send description]
+   * @date 2019-11-09
+   */
+  public function send():void
+  {
+    http_response_code($this->code);
+
+    if ($data != null) echo !$this->json ? $this->data : json_encode($this->data);
+
+    if (get_instance()->config->item('rest')['response_exit']) {
+      exit(EXIT_SUCCESS);
+    }
+  }
 }
+?>
