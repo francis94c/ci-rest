ad8a440e1701a7dd331c598954c7862f8e242d65
Francis Ilechukwu <francis94c@gmail.com>
2020-04-18 11:09:02 +0100

    Doc Blocks and Defaults.

--- libraries/RESTResponse.php
+++ libraries/RESTResponse.php
@@ -13,26 +13,37 @@ class RESTResponse extends CI_Controller
   // 50*
   const INTERNAL_SERVER_ERROR = 500;
   const NOT_IMPLEMENTED       = 501;
+
   /**
    * [protected HTTP Response Code]
    * @var int
    */
   protected $code;
+
   /**
    * [protected Response Data]
    * @var mixed
    */
   protected $data;
+
   /**
    * [protected Shoud Response be JSON Encoded?]
    * @var bool
    */
   protected $json;
-  function __construct($data=null, int $code=null)
+
+  /**
+   * [__construct description]
+   * @date  2020-04-11
+   * @param [type]     $data [description]
+   * @param integer    $code [description]
+   */
+  public function __construct($data=null, ?int $code=null)
   {
     $this->data = $data;
     $this->code = $code;
   }
+
   /**
    * [__toString description]
    * @date   2019-11-09
@@ -40,8 +51,9 @@ class RESTResponse extends CI_Controller
    */
   public function __toString():string
   {
-    return !$this->json ? $this->data : n_encode($this->data);
+    return !$this->json ? $this->data : json_encode($this->data);
   }
+
   /**
    * [json description]
    * @date   2019-11-11
@@ -49,14 +61,14 @@ class RESTResponse extends CI_Controller
    * @param  int          $code [description]
    * @return RESTResponse       [description]
    */
-  public function json($data, int $code):RESTResponse
+  public function json($data, ?int $code=null):RESTResponse
   {
     $this->json = true;
     $this->code = $code;
     $this->data = $data;
     return $this;
   }
-  
+
   /**
    * [send description]
    * @date  2019-11-11
